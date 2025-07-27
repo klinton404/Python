fa54bb33546a7b2db814565899e309575393bef4
Klinton Chakma <159724195+klinton404@users.noreply.github.com>
2025-07-27 22:57:03 +0600

    Add files via upload

---
+++ Default argument 2.py
@@ -0,0 +1,8 @@
+def greet(who="Colin"):
+    print("Hello,", who)
+
+
+greet()
+greet(who="Kaggle")
+# (In this case, we don't need to specify the name of the argument, because it's unambiguous.)
+greet("world")
